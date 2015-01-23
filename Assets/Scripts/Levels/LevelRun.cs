a85cdd1ff68dfae9ed1f0066a301a31da37bb449
brian tria <btria959@gmail.com>
2015-01-24 07:44:52 +0800

    refactor platform fade in

--- Assets/Scripts/Levels/LevelRun.cs
+++ Assets/Scripts/Levels/LevelRun.cs
@@ -1,38 +1,58 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class LevelRun : MonoBehaviour 
 {
 	public Text m_letterW;
-	private float alpha;
 	private bool m_bIsPlatformVisible;
-	private MaterialFadeAnim m_matFadeAnim;
+	private MaterialFadeAnim m_platformMatFadeAnim;
+	
+	private float m_letterFadeInSpeed;
+	Vector4 m_letterColor;
 	
 	#region MONO FUNCTIONS
 	private void Awake ()
 	{
-		alpha = 0.0f;
+		m_letterFadeInSpeed = 0.0f;
+		m_letterColor = m_letterW.color;
 		m_bIsPlatformVisible = false;
-		m_matFadeAnim = this.transform.FindChild("Platform").GetComponent<MaterialFadeAnim>();
+		m_platformMatFadeAnim = this.transform.FindChild("Platform").GetComponent<MaterialFadeAnim>();
+	}
+	
+	private void Start ()
+	{
+		Screen.showCursor = false;
+		FadeIn();
 	}
 	
 	private void Update ()
 	{
+		// TODO: Centralized keyboard input manager
 		if(   m_bIsPlatformVisible == false
 		   && Input.GetKeyUp(KeyCode.W))
 		{
-//			Debug.Log("bakit?????");
 			m_bIsPlatformVisible = true;
-			m_matFadeAnim.FadeIn();
-		}
-	
-		if(alpha < 255)
-		{
-			alpha += 1.2f;
-			Vector4 c = m_letterW.color;
-			c.w += Time.deltaTime * alpha * Constants.ALPHA_RATIO;
-			m_letterW.color = (Color) c;
+			m_platformMatFadeAnim.FadeIn();
 		}
 	}
 	#endregion
+	
+	private void FadeIn (float p_speed = 120.0f)
+	{
+		m_letterFadeInSpeed = p_speed;
+		StartCoroutine("IEFadeIn");
+	}
+	
+	private IEnumerator IEFadeIn ()
+	{
+		while(m_letterColor.w < 1.0f)
+		{
+			m_letterColor.w += (Time.deltaTime * m_letterFadeInSpeed * Constants.ALPHA_RATIO);
+			m_letterW.color = (Color) m_letterColor;
+			yield return 0;
+		}
+		
+		StopCoroutine("IEFadeIn");
+	}
 }
