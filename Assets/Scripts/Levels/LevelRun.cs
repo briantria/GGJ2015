4a114d1d92561f7dc1ff328e71cddd1b39c94de0
brian tria <btria959@gmail.com>
2015-01-24 01:24:01 +0800

    playable level 03

---
+++ Assets/Scripts/Levels/LevelRun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelRun : MonoBehaviour 
+{
+	public Text m_letterW;
+	private float alpha;
+	
+	#region MONO FUNCTIONS
+	private void Awake ()
+	{
+		alpha = 0.0f;
+	}
+	
+	private void Update ()
+	{
+		if(alpha < 255)
+		{
+			alpha += 1.2f;
+			Vector4 c = m_letterW.color;
+			c.w += Time.deltaTime * alpha * 0.0039f; // speed * alpha * (1 / 255)
+			m_letterW.color = (Color) c;
+		}
+	}
+	#endregion
+}
