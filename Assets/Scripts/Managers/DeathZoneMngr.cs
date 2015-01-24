77a480933f73bb02bcf5a286ec9012b559fb3ced
brian tria <btria959@gmail.com>
2015-01-24 23:58:01 +0800

    added death scream

--- Assets/Scripts/Managers/DeathZoneMngr.cs
+++ Assets/Scripts/Managers/DeathZoneMngr.cs
@@ -7,18 +7,22 @@ public class DeathZoneMngr : MonoBehaviour
 	private GameObject m_player;
 	private Image m_fadeScreen;
 	private Vector4 m_screenColor;
+	private AudioSource m_scream;
 	
 	private void Awake ()
 	{
 		m_player = GameObject.FindGameObjectWithTag("Player").gameObject;
 		m_fadeScreen = GameObject.Find("FadeScreen").transform.FindChild("Image").GetComponent<Image>();
 		m_screenColor = m_fadeScreen.color;
+		
+		m_scream = GetComponent<AudioSource>();
 	}
 	
 	private void OnTriggerEnter (Collider p_collider)
 	{
 		if(p_collider.CompareTag("Player"))
 		{
+			m_scream.Play();
 			StartCoroutine("ResetGame");
 		}
 	}
@@ -37,7 +41,7 @@ public class DeathZoneMngr : MonoBehaviour
 		m_player.transform.position = Constants.PLAYER_INITPOS;
 		m_player.transform.localScale = Vector3.one;
 		m_player.transform.eulerAngles = Vector3.zero;
-		yield return new WaitForSeconds(0.3f);
+		yield return new WaitForSeconds(m_scream.clip.length);
 		
 		// fade to black
 		while (m_screenColor.w > 0.0f)
